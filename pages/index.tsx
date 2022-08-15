0757ead3476b612d18ba42ea36b6e306fef8d20b
fmfontana <aniciomanlio01@gmail.com>
2022-08-15 08:55:43 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,11 +13,11 @@ const Home: NextPage = (props) => {
 
 export const getStaticProps: GetStaticProps = async (context) => {
 	const {
-		env: { MARVEL_API_TS, MARVEL_API_KEY, MARVEL_API_HASH },
+		env: { MARVEL_API_TS = null, MARVEL_API_KEY = null, MARVEL_API_HASH = null },
 	} = process;
 
 	const { data } = await marvelApi.get<MarvelResponse>(
-		`/characters?ts=${MARVEL_API_TS}&apikey=${MARVEL_API_KEY}&hash=${MARVEL_API_HASH}&limit=60`
+		`/characters?ts=${MARVEL_API_TS}&apikey=${MARVEL_API_KEY}&hash=${MARVEL_API_HASH}&limit=30`
 	);
 
 	return {
