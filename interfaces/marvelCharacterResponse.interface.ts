e93e8ecff23e794061c20732526520741dd5a8e1
fmfontana <aniciomanlio01@gmail.com>
2022-08-16 08:52:59 -0300

    uptd

--- interfaces/marvelCharacterResponse.interface.ts
+++ interfaces/marvelCharacterResponse.interface.ts
@@ -1,6 +1,4 @@
 export interface MarvelCharacterResponse {
-	code: number;
-	status: string;
 	data: Data;
 }
 
@@ -11,37 +9,20 @@ export interface Data {
 export interface Character {
 	id: number;
 	name: string;
-	description: string;
 	thumbnail: Thumbnail;
-	resourceURI: string;
 	comics: Comics;
 	series: Comics;
-	urls: URL[];
 }
 
 export interface Comics {
-	available: number;
-	collectionURI: string;
 	items: ComicsItem[];
-	returned: number;
 }
 
 export interface ComicsItem {
 	name: string;
-	resourceURI: string;
-}
-
-export interface StoriesItem {
-	name: string;
-	resourceURI: string;
 }
 
 export interface Thumbnail {
 	path: string;
 	extension: string;
-}
-
-export interface URL {
-	type: string;
-	url: string;
 }
--- interfaces/marvelResponse.interface.ts
+++ interfaces/marvelResponse.interface.ts
@@ -1,48 +1,15 @@
 export interface MarvelResponse {
-	code: number;
-	status: string;
-	copyright: string;
-	attributionText: string;
-	attributionHTML: string;
-	etag: string;
 	data: Data;
 }
 
 export interface Data {
-	offset: number;
-	limit: number;
-	total: number;
-	count: number;
 	results: Character[];
 }
 
 export interface Character {
 	id: number;
 	name: string;
-	description: string;
-	modified: string;
 	thumbnail: Thumbnail;
-	resourceURI: string;
-	comics: Comics;
-	series: Comics;
-	urls: URL[];
-}
-
-export interface Comics {
-	available: number;
-	collectionURI: string;
-	items: ComicsItem[];
-	returned: number;
-}
-
-export interface ComicsItem {
-	name: string;
-}
-
-export enum ItemType {
-	Cover = "cover",
-	Empty = "",
-	InteriorStory = "interiorStory",
 }
 
 export interface Thumbnail {
@@ -52,16 +19,5 @@ export interface Thumbnail {
 
 export enum Extension {
 	GIF = "gif",
-	Jpg = "jpg",
-}
-
-export interface URL {
-	type: URLType;
-	url: string;
-}
-
-export enum URLType {
-	Comiclink = "comiclink",
-	Detail = "detail",
-	Wiki = "wiki",
+	JPG = "jpg",
 }
